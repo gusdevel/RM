b277db3be6e971dd2cf0bd640ab892d332b154d1
gustavo.arellano <g.arellanno@gmail.com>
2018-03-02 17:49:35 -0600

    No activa display cuando el equipo esta bloqueado Al cerrar la app, ya no se re-activa por sí sola.
    Se creo un servicio que detecta el cierre de la app, y este será el encargado de revivir la aplicación generando nuevos procesos.
    
    Se habilito el display cuando el equipo se encuentra bloqueado.

--- app/src/main/java/mode/retail/polaroid/mx/retailmode/receiver/ScreenReceiver.java
+++ app/src/main/java/mode/retail/polaroid/mx/retailmode/receiver/ScreenReceiver.java
@@ -4,10 +4,15 @@ import android.app.KeyguardManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.PowerManager;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 import mode.retail.polaroid.mx.retailmode.MainActivity;
 import mode.retail.polaroid.mx.retailmode.RetailModeActivity;
+import mode.retail.polaroid.mx.retailmode.services.TimeService;
 
 public class ScreenReceiver extends BroadcastReceiver {
 
@@ -15,6 +20,32 @@ public class ScreenReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         //String action = intent.getAction();
 
-        RetailModeActivity.clearScreen();
+        //Toast.makeText(context, "Receiver display", Toast.LENGTH_LONG).show();
+
+        if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+            RetailModeActivity.unlockScreen();
+
+            // Unlock the screen
+            PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+            PowerManager.WakeLock wl = pm.newWakeLock(
+                    PowerManager.FULL_WAKE_LOCK
+                            | PowerManager.ACQUIRE_CAUSES_WAKEUP
+                            | PowerManager.ON_AFTER_RELEASE,
+                    "RetailMode");
+            wl.acquire();
+
+            KeyguardManager km = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
+            KeyguardManager.KeyguardLock kl = km.newKeyguardLock("RetailMode");
+            kl.disableKeyguard();
+
+            Intent i = new Intent(context, RetailModeActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            context.startActivity(i);
+            wl.release();
+        } else if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+            Intent i = new Intent(context, RetailModeActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            context.startActivity(i);
+        }
     }
 }
